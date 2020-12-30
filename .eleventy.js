6e5d25c730c7df4c27ac2a751259f5542b527301
Simon Fessehaye <fessehay@ualberta.ca>
2020-12-30 04:37:38 -0700

    PWA support

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
+const pluginPWA = require("eleventy-plugin-pwa")
 const pluginTailwindCSS = require("eleventy-plugin-tailwindcss")
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginTailwindCSS)
+  eleventyConfig.addPlugin(pluginPWA)
 
   eleventyConfig.addPassthroughCopy("src/img")
   eleventyConfig.addPassthroughCopy("src/js")
+  eleventyConfig.addPassthroughCopy("manifest.json")
 
   return {
     dir: {
--- src/projects/portfolio.md
+++ src/projects/portfolio.md
@@ -4,5 +4,5 @@ repo: https://github.com/fessehaye/portfolio
 demo: https://fessehaye.netlify.app/
 preview: /img/current.png
 description: The site that you are looking at!
-keywords: ["Eleventy", "Greensock", "Tailwind"]
+keywords: ["Eleventy", "Greensock", "Tailwind", "PWA"]
 ---
