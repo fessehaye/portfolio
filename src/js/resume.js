9321483a4a778029b92ab8c7c5b1fa27db9da2a4
fessehaye <fessehay@ualberta.ca>
2024-08-29 00:11:36 -0700

    update

--- src/js/resume.js
+++ src/js/resume.js
@@ -1,26 +1,4 @@
 ;(function () {
-  document.body.classList.remove("hidden")
-  const tl = gsap.timeline({ paused: true })
-
-  tl.from("nav", {
-    opacity: 0,
-    translateY: -20,
-    duration: 0.3,
-    transformOrigin: "center",
-    ease: "power2.inOut",
-  })
-
-  tl.from("main > div > *", {
-    opacity: 0,
-    translateY: -20,
-    duration: 0.3,
-    transformOrigin: "center",
-    ease: "power2.inOut",
-    stagger: 0.2,
-    clearProps: "scale",
-  })
-
-  tl.play()
 
   const tl_nav = gsap.timeline({ paused: true })
 
@@ -83,61 +61,4 @@
     tl_mobile.reverse(0)
   })
 
-  let tl_page_home, tl_page_other
-
-  window.addEventListener("pagehide", function (event) {
-    if (event.persisted === true) {
-      if (tl_page_home && tl_page_home.progress() === 1) {
-        tl_page_home.seek(0)
-        tl_page_home.pause(0)
-        gsap.set("body > div", { background: "#6366F1" })
-      }
-      if (tl_page_other && tl_page_other.progress() === 1) {
-        tl_page_other.seek(0)
-        tl_page_other.pause(0)
-      }
-    }
-  })
-
-  document.querySelector(".home-link").addEventListener("click", event => {
-    event.preventDefault()
-
-    tl_page_home = gsap.timeline({ paused: true })
-    const mediaQuery = window.matchMedia("(min-width: 1280px)")
-
-    tl_page_home.to("main,nav", {
-      duration: 0.3,
-      opacity: 0,
-    })
-
-    tl_page_home.to("body > div", {
-      duration: mediaQuery.matches ? 0.3 : 0,
-      background: mediaQuery.matches ? "#ffffff" : "#6366F1",
-      onComplete: () => {
-        window.location.href = event.target.href
-      },
-    })
-
-    tl_page_home.play()
-  })
-
-  const allLinks = document.querySelectorAll(".mobile-nav,.nav-link")
-
-  for (const link of allLinks) {
-    link.addEventListener("click", event => {
-      event.preventDefault()
-
-      tl_page_other = gsap.timeline({ paused: true })
-
-      tl_page_other.to("main > div > *,nav", {
-        duration: 0.5,
-        opacity: 0,
-        onComplete: () => {
-          window.location.href = event.target.href
-        },
-      })
-
-      tl_page_other.play()
-    })
-  }
 })()
